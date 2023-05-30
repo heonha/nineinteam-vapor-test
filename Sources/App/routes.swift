3cf3b98c5edb01cb977e86bad116accc82c17f5a
HeonJin Ha <weizen511@gmail.com>
2023-05-30 18:59:35 +0900

    chore: github webhook 분리

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,24 +1,5 @@
 import Vapor
 
-func restartServer(app: Application) throws {
-    // 서버 중지
-    app.shutdown()
-
-    // 서버 재시작 명령 실행
-    let vaporPath = "/path/to/vapor"  // Vapor 실행 파일 경로
-    let arguments = ["run"]  // Vapor 명령 인수
-    let process = Process()
-    process.executableURL = URL(fileURLWithPath: vaporPath)
-    process.arguments = arguments
-
-    do {
-        try process.run()
-    } catch {
-        // 서버 재시작 실패
-        print("Failed to restart server: \(error)")
-    }
-}
-
 func routes(_ app: Application) throws {
 
     app.middleware.use(ErrorMiddleware())
@@ -26,10 +7,6 @@ func routes(_ app: Application) throws {
     app.get { req async in
         "Server is working!"
     }
-
-    let gitHubWebhookController = GitHubWebhookController()
-    app.post("github-webhook", use: gitHubWebhookController.handle)
-
 
     let teamsController = TeamsController()
     app.get("teams", use: teamsController.getTeams)
